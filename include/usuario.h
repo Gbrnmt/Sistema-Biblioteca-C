c773d5fbf2d1ec375d95656c0b3fc414a623a648
Gasparzin-99 <dacostameloian@gmail.com>
2026-09-25 15:43:02 -0300

    implementa cadastro e persistencia de usuarios
    implementa cadastro e persistencia de usuarios

--- include/usuario.h
+++ include/usuario.h
@@ -4,19 +4,37 @@
 #define MAX_USUARIOS 100
 
 typedef struct {
+
     int matricula;
+
     char nome[100];
+
     char curso[100];
+
 } Usuario;
 
-void cadastrarUsuario(Usuario usuarios[], int *total);
+void cadastrarUsuario(
+    Usuario usuarios[],
+    int *total
+);
 
-void listarUsuarios(Usuario usuarios[], int total);
+void listarUsuarios(
+    Usuario usuarios[],
+    int total
+);
 
-void buscarUsuario(Usuario usuarios[], int total);
+void buscarUsuario(
+    Usuario usuarios[],
+    int total
+);
 
-void salvarUsuarios(Usuario usuarios[], int total);
+void salvarUsuarios(
+    Usuario usuarios[],
+    int total
+);
 
-int carregarUsuarios(Usuario usuarios[]);
+int carregarUsuarios(
+    Usuario usuarios[]
+);
 
 #endif
