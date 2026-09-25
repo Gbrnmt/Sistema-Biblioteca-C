a0fda4945620b1f72f49f33848aa255796be5926
Gbrnmt <gabrielnepomucenomanta11@gmail.com>
2026-09-25 14:21:21 -0300

    implementa cadastro e persistencia de livros
    implementa cadastro e persistencia de livros

--- include/livro.h
+++ include/livro.h
@@ -5,25 +5,38 @@
 
 // Estrutura que representa um livro
 typedef struct {
+
     int codigo;
+
     char titulo[100];
+
     char autor[100];
+
     int ano;
+
     int quantidade;
+
 } Livro;
 
 // Cadastro e listagem
 void cadastrarLivro(Livro livros[], int *total);
+
 void listarLivros(Livro livros[], int total);
 
 // Busca
 void buscarLivro(Livro livros[], int total);
 
 // Alterar quantidade
-void alterarQuantidade(Livro livros[], int total, int codigo, int quantidade);
+void alterarQuantidade(
+    Livro livros[],
+    int total,
+    int codigo,
+    int quantidade
+);
 
 // Arquivo
 void salvarLivros(Livro livros[], int total);
+
 int carregarLivros(Livro livros[]);
 
 #endif
